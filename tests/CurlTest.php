e8042566efe31f9af7a8406c10aaefd6aa6739c2
HikaruTakahashi <hikaru@hikaru-takahashi-no-MacBook.local>
2016-03-19 21:11:12 +0900

    add test code

---
+++ tests/CurlTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class CurlTest extends PHPUnit_Framework_TestCase
+{
+    public function testGet()
+    {
+    }
+    
+    public function testPost()
+    {
+    }
+    
+    public function testPut()
+    {
+    }
+
+}
