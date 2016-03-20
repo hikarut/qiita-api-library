b1301cb0d11b935f1587b029033f09b40bbf9c0f
HikaruTakahashi <hikaru@hikaru-takahashi-no-MacBook.local>
2016-03-20 15:48:17 +0900

    failed test

--- tests/CurlTest.php
+++ tests/CurlTest.php
@@ -4,14 +4,17 @@ class CurlTest extends PHPUnit_Framework_TestCase
 {
     public function testGet()
     {
+        $this->assertTrue(true);
     }
     
     public function testPost()
     {
+        $this->assertTrue(true);
     }
     
     public function testPut()
     {
+        $this->assertTrue(false);
     }
 
 }
