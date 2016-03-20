be9472e38f16cd35324f60ee96cb8e66d5d739dd
HikaruTakahashi <hikaru@hikaru-takahashi-no-MacBook.local>
2016-03-20 18:30:13 +0900

    add anotation

--- tests/CurlTest.php
+++ tests/CurlTest.php
@@ -2,16 +2,25 @@
 
 class CurlTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers Curl::get
+     */
     public function testGet()
     {
         $this->assertTrue(true);
     }
     
+    /**
+     * @covers Curl::post
+     */
     public function testPost()
     {
         $this->assertTrue(true);
     }
     
+    /**
+     * @covers Curl::put
+     */
     public function testPut()
     {
         $this->assertTrue(true);
